326fa0efe6ff310f21752cc9a0b707f4565627dd
mdajgarhossain <mdajgarhossain1@gmail.com>
2021-04-15 16:33:07 +0600

    Add a tag inside transition-group to render slider elements

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,41 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <transition-group tag="div">
+      <div v-for="i in [currentIndex]" :key="i">
+        <img :src="currentImage" />
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Slider',
+  data() {
+    return {
+      images: [
+        'https://cdn.pixabay.com/photo/2021/03/12/12/28/beach-6089501_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2021/04/11/12/11/plum-blossom-6169571_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2021/01/11/08/53/sky-5907605_960_720.jpg'
+      ],
+      currentIndex: 0,
+    }
+  },
+   computed: {
+     currentImage() {
+       return this.images && this.images.length && this.images[Math.abs(this.currentIndex) % this.images.length];
+     }
+   }
 }
 </script>
 
